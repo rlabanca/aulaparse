3eb98d6996e9c64dcdc09e5ddc41455719bc4a00
Rodrigo Labanca <rodrigo.labanca@gmail.com>
2015-06-18 19:32:58 -0300

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Rodrigo Labanca on 6/18/15.
+//  Copyright (c) 2015 Rodrigo Labanca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
